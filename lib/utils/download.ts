73c86259011a334d2d1529d608773dbf224b2404
iblabd <iqbalabdurrahman04@gmail.com>
2024-12-29 09:53:46 +0700

    Change file naming

--- lib/utils/download.ts
+++ lib/utils/download.ts
@@ -8,13 +8,13 @@ export const handleImageDownload = async ({ id }: { id: string }) => {
     windowWidth: 1920,
     windowHeight: 1080,
   })
-  const data = canvas.toDataURL('image/jpg')
+  const data = canvas.toDataURL('image/png')
   const link = document.createElement('a')
 
   link.href = data
   const now = new Date()
   const timestamp = now.getTime() / 1000
-  const filename = `wraptify_${id}_${Math.floor(timestamp)}.jpg`
+  const filename = `wraptify_${Math.floor(timestamp)}`
   link.download = filename
 
   document.body.appendChild(link)
